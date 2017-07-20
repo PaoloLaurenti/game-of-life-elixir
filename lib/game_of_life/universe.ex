007478ec5ba74b9b4d017999e1ec728ffc8d2cc5
Paolo Laurenti <laurentipaolo@gmail.com>
2017-07-20 19:11:25 +0200

    Simplify neighbours retrieving made by Universe

--- lib/game_of_life/universe.ex
+++ lib/game_of_life/universe.ex
@@ -16,29 +16,11 @@ defmodule GameOfLife.Universe do
 
   defp get_all_neighbours(universe, cell_coordinate) do
     Enum.reduce(@neighbours_places, [], fn(p, n_acc) ->
-      case get_all_neighbours(universe, cell_coordinate, p) do
+      case get_neighbour(universe, cell_coordinate, p) do
         nil -> n_acc
         neighbour -> [neighbour | n_acc]
       end
     end)
-  end
-
-  defp get_all_neighbours(_universe, {_, 0}, p) when p in [:top_left, :top, :top_right], do: nil
-  defp get_all_neighbours(_universe, {0, _}, p) when p in [:top_left, :left, :bottom_left], do: nil
-  defp get_all_neighbours(universe, cell_coordinate, p) do
-    universe_x_size = universe[0] |> Map.keys |> Enum.count
-    universe_y_size = universe |> Map.keys |> Enum.count
-
-    case {cell_coordinate, p} do
-      {{^universe_x_size, ^universe_y_size}, _} -> nil
-      {{^universe_x_size, _}, :top_right} -> nil
-      {{^universe_x_size, _}, :right} -> nil
-      {{^universe_x_size, _}, :bottom_right} -> nil
-      {{_, ^universe_y_size}, :bottom_left} -> nil
-      {{_, ^universe_y_size}, :bottom} -> nil
-      {{_, ^universe_y_size}, :bottom_right} -> nil
-      {_, _} -> get_neighbour(universe, cell_coordinate, p)
-    end
   end
 
   defp get_neighbour(universe, {x, y}, :top_left), do: universe[x - 1][y - 1]
